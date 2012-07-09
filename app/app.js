508b553149845983edebb551603ee831e8cbc979
Antoine Blanchet <antoine.blanchet@invenietis.com>
2012-07-09 12:17:48 +0200

    add jade and basic app squeleton

--- app/app.js
+++ app/app.js
@@ -1,9 +1,25 @@
 var express = require('express');
 
+// Create express application
 var app = express.createServer();
 
-app.get('/', function (req, res) {
-    res.send('hello world');
+// Configuration
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
+// basic routes
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.get('/test', function (req, res) {
+    res.send('lolol');
+});
+
+// start listening
 app.listen('9090');
