9acce3053594ef47e9b915896568b98c6be7b9c0
Antoine Blanchet <antoine.blanchet@invenietis.com>
2012-07-09 12:31:30 +0200

    load external configuration

--- app/app.js
+++ app/app.js
@@ -3,6 +3,18 @@ var express = require('express');
 // Create express application
 var app = express.createServer();
 
+// Get configuration : call looks like : node app.js KEY=1234 SECRET=123456
+var cfg =
+{
+    trello: 
+    {
+        key : process.argv[2].substring(4),
+        secret : process.argv[3].substring(7)
+    }
+};
+
+console.log(cfg);
+
 // Configuration
 app.configure(function () {
     app.set('views', __dirname + '/views');
@@ -12,6 +24,7 @@ app.configure(function () {
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
+
 
 // basic routes
 app.get('/', function (req, res) {
