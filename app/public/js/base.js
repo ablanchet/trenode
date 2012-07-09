f2d9facb5be4eb387644c5535fe90b6fcbf912b4
Denis Martinho <denis.fmartinho@gmail.com>
2012-07-09 15:28:02 +0200

    Adding input for searching labels

--- app/public/js/base.js
+++ app/public/js/base.js
@@ -1,3 +1,9 @@
 ﻿$(function () {
     console.log('jquery is here ? :' + $);
+
+    $('input[name=searchBtn]').click(function () {
+        if ($('input[name=labelTb]').val() != 'undefined') {
+            window.location = "/cards/label/" + $('input[name=labelTb]').val();
+        }
+    });
 });
